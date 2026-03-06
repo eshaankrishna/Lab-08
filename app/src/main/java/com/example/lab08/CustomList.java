7145ccbe20b78d23f0402bff0292b0b01731522e
eshaankrishna <eshaankr@ualberta.ca>
2026-03-07 01:59:25 +0530

    Implement hasCity method

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -14,8 +14,7 @@ public class CustomList {
         cities.add(city);
     }
 
-    // testing CI
-
-    // Will be implemented later using TDD (leave blank for now)
-    // public boolean hasCity(City city) { ... }
+    public boolean hasCity(City city) {
+        return cities.contains(city);
+    }
 }
